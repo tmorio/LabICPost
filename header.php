cadf710ab50c4154597573582732d7542a7f7caa
tmorio <morikapusan@eleken.jp>
2019-06-14 19:29:51 +0900

    Ver1.0

---
+++ header.php
@@ -0,0 +1,31 @@
+<div class="serviceHeader navbar-fixed">
+	<nav>
+		<div class="nav-wrapper black-text">
+			<!-- ロゴ -->
+			<a href="dashboard.php"><img class="logo-image" src="img/logo.png"></a>
+			<ul class="right">
+					<?php
+					if(!empty($_SESSION['userName'])){
+						echo '<div class="chip">';
+						if(!empty($_SESSION['PhotoName'])){
+							echo '<img src="img/users/' . $_SESSION['PhotoName'] . '.jpg" alt="Contact Person">';
+						}else{
+							echo '<img src="img/default.jpg" alt="Contact Person">';
+						}
+					}
+					?>
+					<?php
+						if(!empty($_SESSION['userName'])){
+							print htmlspecialchars($_SESSION['userName'], ENT_QUOTES, 'UTF-8');
+							echo '</div>';
+						}
+					?>
+					&nbsp;&nbsp;
+				&thinsp;
+			</ul>
+		</div>
+	</nav>
+</div>
+<script>
+	$('.dropdown-trigger').dropdown();
+</script>
---
+++ myid.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'hoge');
+define('DB_ID', 'hogehoge');
+define('DB_PASS', 'example');
+
+define('SERVER_DOMAIN', 'example.com');
